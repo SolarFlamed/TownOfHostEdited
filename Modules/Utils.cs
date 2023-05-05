0bfbc1fd4ca525ced93576ccae41413cfe2283ac
joeyjanekey <ghostax02@gmail.com>
2023-05-05 09:30:02 +0700

    version info

--- Modules/Utils.cs
+++ Modules/Utils.cs
@@ -902,7 +902,7 @@ public static class Utils
             if (player.AmOwner)
             {
                 if (GameStates.IsOnlineGame)
-                    name = $"<color=#ffd6ec>TOHE</color><color=#baf7ca>★</color>" + name;
+                    name = $"<color=#902efd>Host</color><color=#4bf4ff>♥</color>" + name;
                 if (Options.CurrentGameMode == CustomGameMode.SoloKombat)
                     name = $"<color=#f55252><size=1.9>{GetString("ModeSoloKombat")}</size></color>\r\n" + name;
             }
